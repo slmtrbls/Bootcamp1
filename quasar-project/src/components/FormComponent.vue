76f08dd12eabb65ab0c5bf238344c28199ff17ce
slmtrbls <slmtrbls@gmail.com>
2023-04-14 11:54:46 +0200

    update

---
+++ quasar-project/src/components/FormComponent.vue
@@ -0,0 +1,57 @@
+<script>
+let id = 0
+
+export default {
+  data() {
+    return {
+      newTechno: '',
+      technos: [
+        { id: id++, text: 'TypeScript' },
+        { id: id++, text: 'HTML' },
+        { id: id++, text: 'HTML' }
+      ]
+    }
+  },
+  methods: {
+    addTechno() {
+      this.technos.push({ id: id++, text: this.newTechno })
+      this.newTechno = ''
+    },
+    removeTechno(techno) {
+      this.technos = this.technos.filter((t) => t !== techno)
+    }
+  }
+}
+</script>
+
+<template>
+    <h2>Your favorite techno</h2>
+    <form @submit.prevent="addTechno">
+        <input v-model="newTechno" placeholder="Write your favorite techno">
+        <br/>
+        <button>Add</button>    
+    </form>
+    <ul>
+        <li v-for="techno in technos" :key="techno.id">
+        {{ techno.text }}
+        <button @click="removeTechno(techno)">X</button>
+        </li>
+    </ul>
+</template>
+
+<style>
+h2 {
+    font-size: 20px;
+    margin-bottom: 30px;
+}
+
+input {
+    border : none;
+    border-bottom: 1px solid black;
+    margin-bottom: 5px;
+}
+
+ul {
+    margin-left: -60%;
+}
+</style>
--- quasar-project/src/layouts/MainLayout.vue
+++ quasar-project/src/layouts/MainLayout.vue
@@ -1,19 +1,28 @@
 <template>
   <h1>My Quasar Project</h1>
-  <div v-for="techno in technos" :key="techno.id">
-    <CardComponent :label="techno.label" :text="techno.text" />
+  <div id="SiteWrapper">
+    <div id="CardContainer">
+      <div v-for="techno in technos" :key="techno.id">
+        <CardComponent :label="techno.label" :text="techno.text" />
+      </div>
+    </div>
+    <div id="FormContainer">
+      <FormComponent />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 
 import CardComponent from 'src/components/CardComponent.vue'
+import FormComponent from 'src/components/FormComponent.vue'
 import * as techno from 'src/assets/data.json'
 
 export default {
   name: 'MainLayout',
   components: {
-    CardComponent
+    CardComponent,
+    FormComponent
   } ,
   data() {
     return {
@@ -30,5 +39,25 @@ export default {
 h1 {
   font-size: 40px;
   text-align: center;
+}
+
+#SiteWrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+#CardContainer {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 70%;
+}
+
+#FormContainer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin:0 10% 0 0;
 }
 </style>
